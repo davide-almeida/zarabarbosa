a25fce2638e73aa9009922f0494eeb336e09a5b8
Davide Almeida <davide.jhonatan@gmail.com>
2019-06-02 16:25:08 +0000

    Fix question table

--- db/migrate/20180923233947_create_questions.rb
+++ db/migrate/20180923233947_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
       t.string :doubt
-      t.string :answer
+      t.text :answer
 
       t.timestamps
     end
