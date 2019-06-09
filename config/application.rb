30f15e2e0aafe991f1d379f92b3e750dbcd11d55
Davide Almeida <davide.jhonatan@gmail.com>
2019-06-09 12:16:54 +0000

    test application.rb config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module Zarabarbosa
     config.i18n.default_locale = :"pt-BR"
     config.time_zone = 'Brasilia'
 
+    config.i18n.fallbacks = {
+      de: :en
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
