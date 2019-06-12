cb4d55575b7da53767f36f1e76b3b023b8708a28
Davide Almeida <davide.jhonatan@gmail.com>
2019-06-12 02:23:20 +0000

    fix rails-erd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
   #BetterErrors config
-  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
   # Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
