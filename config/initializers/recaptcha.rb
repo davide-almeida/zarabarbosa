9cb174f3c7673b7a08b164ab936ae23ab8c1d358
Davide Almeida <davide.jhonatan@gmail.com>
2019-12-21 17:23:06 +0000

    fix captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 # reCAPTCHA
 Recaptcha.configure do |config|
-    config.site_key="6Lc7E8kUAAAAAENWNSX5RQOaf2es4HHRVnPp2MFP"
-    config.secret_key="6Lc7E8kUAAAAAIJCfQl66a-KlIe90OusuRsvyBJr"
+    config.site_key="6LcyFckUAAAAAG1w7fAeHZkFM5W6-tUr7nW0cYoD"
+    config.secret_key="6LcyFckUAAAAAJMdB8SM-i3wANjeO5A3EUuIKwnd"
     # RECAPTCHA_SITE_KEY: '6Lc7E8kUAAAAAENWNSX5RQOaf2es4HHRVnPp2MFP'
     # RECAPTCHA_SECRET_KEY: '6Lc7E8kUAAAAAIJCfQl66a-KlIe90OusuRsvyBJr'
 end
