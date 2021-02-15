39dde8895f0ac93dd807258e0228e0068669acf0
Davide Almeida <davide.jhonatan@gmail.com>
2021-02-15 11:55:48 -0300

    fix ssl config

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Zarabarbosa
     #   de: :en
     # }
 
+    # set config ssl
+    config.action_controller.forgery_protection_origin_check = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
